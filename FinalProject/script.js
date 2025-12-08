d38441d3cf31d387fc5ea5bf46ec6870f94826a0
Bianca <bpowersau@gmail.com>
2025-12-07 21:43:43 -0800

    Setup html, CSS, and JS structure

---
+++ FinalProject/script.js
@@ -0,0 +1,36 @@
+// Anonymous fuction = function without a name
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+
+    class InputHandler {
+        constructor() {
+            this.keys = [];
+            window.addEventListener('keydown', function(){
+                console.log(e);
+            });
+        }
+    }
+
+    class Player {
+    }
+
+    class Background {
+    }
+
+    class Enemy {
+    }
+
+    function handleEnemies() {
+    }
+
+    function displayStatusText() {
+    }
+
+    const input = new InputHandler();
+
+    function animate() {
+    }
+});
